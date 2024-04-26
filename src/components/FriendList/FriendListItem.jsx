b07e20b314013e5a76e51b613e6f2cfeee468fae
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-26 21:14:56 +0300

    stopped on clsx

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,18 @@
 import s from './FriendListItem.module.css';
+import clsx from 'clsx';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img className={s.image} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
-      <p className={s.status}>{isOnline} Status</p>
+      <p
+        className={clsx({
+          [s.isOnline]: isOnline,
+        })}
+      >
+        {isOnline} Status
+      </p>
     </div>
   );
 };
